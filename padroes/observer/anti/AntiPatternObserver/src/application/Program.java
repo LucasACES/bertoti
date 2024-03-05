ac9222d3db04a8cb1faee72306685247f227a597
LucasACES <lucasc.a01@outlook.com>
2024-03-05 20:53:51 -0300

    update

---
+++ padroes/observer/anti/AntiPatternObserver/src/application/Program.java
@@ -0,0 +1,13 @@
+package application;
+
+import model.entities.Carro;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Carro car = new Carro();
+		car.setInfo("eee343", "vw", 40.45);
+
+	}
+
+}
